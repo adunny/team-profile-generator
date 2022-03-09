3c34261322c748fe25f7f8c27ed2a974316ad874
alen <alen.dunn1@gmail.com>
2022-03-09 15:47:00 -0500

    formatting/cleaning up files

--- Develop/lib/renderPromise.js
+++ Develop/lib/renderPromise.js
@@ -6,21 +6,21 @@ const writeHtml = employees => {
     return new Promise((resolve, reject) => {
         resolve(render(employees));
     })
-    .then(html => {
-        fs.writeFile("./dist/team.html", html, err => {
-            if (err) {
-                console.log(err)
-            }
-            else {
-                console.log(`
+        .then(html => {
+            fs.writeFile("./dist/team.html", html, err => {
+                if (err) {
+                    console.log(err)
+                }
+                else {
+                    console.log(`
                 =================================
                 Team created! Check /dist folder.
                 =================================
                 `)
-            }
-        })
+                }
+            })
 
-    })
+        })
 }
 
 
